ad26b9ceaadae807d54575d01f220a99d3af061d
taovuzu <yatindanger0001@gmail.com>
2025-05-16 17:29:00 +0000

    feat(server): Add validation logic for API requests

---
+++ server/src/validators/user.validator.js
@@ -0,0 +1,114 @@
+import { body, query } from "express-validator";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const emailValidator = () => {
+  return [];
+
+
+
+
+
+
+
+
+};
+
+const usernameValidator = () => {
+  return [];
+
+
+
+
+
+
+
+};
+
+const passwordValidator = () => {
+  return [];
+
+
+
+
+
+};
+
+const userLoginValidator = () => {
+  return [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+const userRegisterValidator = () => {
+  return [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+const changeCurrentPasswordValidator = () => {
+  return [];
+
+
+
+
+
+
+
+
+
+};
+
+const resetForgottenPasswordValidator = () => {
+  return [];
+
+
+
+
+
+
+
+
+};
+
+export {
+  emailValidator,
+  usernameValidator,
+  passwordValidator,
+  userLoginValidator,
+  userRegisterValidator,
+  changeCurrentPasswordValidator,
+  resetForgottenPasswordValidator };
